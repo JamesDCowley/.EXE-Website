39aa8ad44adc21c2c93ce2642fd7fc895fed6454
JamesDCowley <wuz7@txstate.edu>
2024-07-05 21:42:52 -0500

    event cards work with events.json

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ document.addEventListener('DOMContentLoaded', function(){
         events.forEach(event => {
             const eventCard = document.createElement("div");
             eventCard.className = "col-md-4 event-card";
-            eventCard.innerHTML = '\
-                <div class="card">\
-                    <div class="card-body">\
-                        <h5 class="card-title event-card-title">${event.title}</h5>\
-                        <h6 class="card-subtitle mb-2 text-muted">${new Date(event.date).toStringDate()}</h6>\
-                        <p class="card-text event-card-text">${event.description}</p>\
-                    </div>\
-                </div>\
-            ';
+            eventCard.innerHTML = `
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title event-card-title">${event.title}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">${new Date(event.date).toDateString()}</h6>
+                  <p class="card-text event-card-text">${event.description}</p>
+                </div>
+              </div>
+            `;
             eventContainer.appendChild(eventCard);
 
         });
